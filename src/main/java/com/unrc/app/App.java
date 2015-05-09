d9baade7e3d4b425987949fadae209b87b112c70
Mariano Ontivero <orevitnoonairam@yahoo.com.ar>
2015-05-09 20:39:02 -0300

    Chequear barrido de matriz - Adaptar a 6x7 (original de 7x6)

--- src/main/java/com/unrc/app/App.java
+++ src/main/java/com/unrc/app/App.java
@@ -2,14 +2,9 @@ package com.unrc.app;
 
 import com.unrc.app.User;
 import org.javalite.activejdbc.Base;
-import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JTextField;
-import javax.swing.WindowConstants;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * Hello world!
@@ -17,7 +12,8 @@ import javax.swing.WindowConstants;
  */
 public class App
 {
-
+    
+    int turn = 0;			// jugador1: turno par, jugador2: turno impar
     public static void main( String[] args )
     {
         System.out.println( "Bienvenidos a 4 en linea" );
@@ -25,10 +21,95 @@ public class App
 	
 	//Login log = new Login();
 	//log.show_login();
+           try {
+		 int op = menu();
+		 //while (op == 1) {  // mientras sea opcion 1, 2 o 3
+		    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		      //FourInLineState st = new FourInLineState();	// nuevo objeto estado concreto
+		      //FourInLineProblem p = new FourInLineProblem();	// nuevo objeto problema concreto
+                      Board b = new Board(6,7);
+                      //System.out.println(b.toString());
+                      BoardControl bc =new BoardControl(b);
+		      //int depth = op * 3 + 1;	// profundidad por nivel: 4(facil), 7(medio) y 10(dificil)
+		      //MaxMinABEngine<FourInLineProblem,FourInLineState> engine = new MaxMinABEngine<FourInLineProblem,FourInLineState>(p,depth);
+		      //int[] columnA = new int[7];  // arreglo para almacenar la cantidad de elementos de cada columna
+		      int column;
+		      int turn = 0;			// jugador: turno par, maquina: turno impar
+		      boolean player = false; 	// variable para decicir quien jugo
+                      int value = 1;
+                                           
+		      do {
 
- 
-	Base.close();
+                          
+			    //if (turn%2 == 0) {
+				  System.out.println(b.toString()); // muestra Tablero antes de jugar
+				  b.toString();
+                                  boolean option1;
+				  boolean option2 = false;
+				  do { // cicla hasta obtener valor válido
+					System.out.print("Ingrese la columna donde desea colocar una ficha: ");
+					column = new Integer(br.readLine());
+					option1 = (column > 7 || column < 1);
+					if (option1)	// si el usuario elije una columna no valida
+					     System.out.println("Posicion no valida. Intente nuevamente.");
+					else {
+					     option2 = ((bc.getColumnTopValue(column-1)) >= 6);
+					     if (option2)	// si la columna esta llena
+						   System.out.println("\nColumna llena. Intente nuevamente");
+					}
+				  } while (option1 || option2);
+				  column--;
+                                  //System.out.println(value + " " + rowCorrection + " " + column);
+				  bc.insertCoin(value, column);
+                                  //st.setState(column);  // modifica el estado segun la columna que elije el usuario
+				  // player = true;
+                                  player = !player;
+                                  value = value * (-1);
+                                  //rowCorrection = rowCorrection + 2;
+			    } while (!bc.end());
+                              //(!p.end(st));
+		      //System.out.println(st.toString()); 	// muestra Tablero final
+		     // if (turn>=(7*6) && (p.value(st)==0)) // si ya se ocuparon las 42 pocisiones y la diferencia es 0
+                    if (turn>=(6*7)) 
+                        System.out.println("*** Empate ***");
+		    else {   // hay un ganador (jugador u maquina)
+			    if (player) 	// si gano el jugador
+				  System.out.println("*** ¡Ganó el jugador #1! ***");
+			    else 		// si gano la maquina
+				  System.out.println("*** ¡Ganó el jugador #2! ***");
+		      }
+		      //engine.report(); 	// informa la cantidad de podas realizadas durante el juego
+		   //   op = menu(); 		//  vuelve a  pedir la opcion
+		 //}
+	   }
+	   catch (java.io.IOException ioex) {
+		 System.out.println("Error I/O");
+	   }
+        Base.close();
 	System.out.println("Fin de 4 en linea");
+     }
 
-    }
+     /**
+      * Muestra por pantalla el menu y lee por teclado la opcion ingresada por el usuario.
+      * @throws IOException cuando el metodo readLine() falla.
+      * @pre. true.
+      * @post. Muestra por pantalla el menu y lee por teclado la opcion ingresada por el usuario.
+      */
+     private static int menu() throws IOException {
+	   int op = 0;
+	   BufferedReader b = new BufferedReader(new InputStreamReader(System.in));
+	   System.out.println("\n \t -------------------------------");
+	   System.out.println("\t|***** Four In A Line Game *****|");
+	   System.out.println("\t --------------------------------");
+	   System.out.print("\tPresione 1 para comenzar el juego u otro numero para salir: ");
+	   try {
+		 op = new Integer(b.readLine());
+	   }
+	   catch (IOException e) {
+		 throw new IOException("Excepcion bufferedReader.readLine()");
+	   }
+	   return op;
+     }
+
+
 }
--- src/main/java/com/unrc/app/BoardControl.java
+++ src/main/java/com/unrc/app/BoardControl.java
@@ -3,11 +3,11 @@ package com.unrc.app;
 
 
 public class BoardControl {
-
-	int [][] board;       //matriz
-	int [] columnTop;     //cantidad de fichas que tiene la columna, como maximo 7.
-	static final int m=6; //numero de filas
-	static final int n=7; //numero de columnas
+        private Board b;
+        private int [][] table;
+	private int [] columnTop;     //cantidad de fichas que tiene la columna, como maximo 7.
+        private int [] rowToInsert;
+        private boolean currentPlayer; // false = jugdor#1, true = jugador#2
 	//Jugador 1=  1   Jugador 2= -1  El numero 0 representa la celda vacia. 
 	// Nota para el sistema la columna empieza en cero y para el usuario en 1
 	// Para esto hay que hacer una simple convercion colum + 1
@@ -16,36 +16,70 @@ public class BoardControl {
 
 
 	
-	public  BoardControl() {
-		board= new int[m][n];
-		columnTop= new int[n];
+	public  BoardControl(Board b) {
+            this.b = b;
+            table = b.getGrid();
+	    columnTop= new int[b.getNumCol()];
+            rowToInsert = new int[b.getNumCol()];
+            for (int i=0; i<b.getNumCol(); i++) {
+                rowToInsert [i] = b.getNumRow() - 1;
+            }
 	}
+        
+        public int getColumnTopValue(int v) {
+            return columnTop[v];
+}
 
         
         //Ver en grupo: Si es necesario recorrer todas las columnas
         //sino observar si hay espacio
         //en donde debo colocar!!!
         //tampoco insertar cartelitos
-	public boolean hayLugar() {
-		boolean haylugar=false;
-		for (int i=0; i<n;i++)
-			if (columnTop[i]<n) haylugar=true;
-		return haylugar;
-	}
+	//public boolean hayLugar() {
+	//	boolean haylugar=false;
+	//	for (int i=0; i<n;i++)
+	//		if (columnTop[i]<n) haylugar=true;
+	//	return haylugar;
+	//}
 
 
-	public boolean insertCoin(int player_value, int column){
-		if (!hayLugar()) System.out.println("Tablero Lleno");
-		boolean result=false;
+	public void insertCoin(int player_value, int column){
+            //int numRow = b.getNumRow();
+		//if (!hayLugar()) System.out.println("Tablero Lleno");
+		//boolean result=false;
 		//Recibe datos de columnas validas: 0..n-1
 
-		if (columnTop[column] < m) {
-
-			board[columnTop[column]][column]= player_value; //1 o -1 
+		//if (columnTop[column] < m) {
+                        //System.out.println(columnTop[column] + " * " + column + " * " + player_value + rowCorrection);
+                     
+			b.setGrid(rowToInsert[column], column, player_value); //1 o -1 
 			++columnTop[column];
-			result=true;	
+                        --rowToInsert[column];
+                          System.out.println(columnTop[column] + " * " + column + " * " + player_value);
+                          System.out.println();
+			//result=true;	
+		//}
+		//return result;		
+	}
+        
+        	/**
+	 * Indica si un estado dado es un estado final, es decir, un estado sin sucesores. 
+	 * Esto ocurre cuando el tablero esta lleno o alguno de los jugadores logro alinear 
+	 * cuatro fichas consecutivas, ya sea en forma vertical, horizontal o diagonal.
+	 * @param state es el estado que se analizara para ver si es un estado final.
+	 * @return true ssi state es un estado final.
+	 * @pre. state!=null.
+	 * @post. true es retornado ssi state es un estado final.
+	 */
+	public boolean end() {
+		int rdo;
+		if (!currentPlayer) {
+			rdo = maxLine(1);
 		}
-		return result;		
+		else {
+			rdo = maxLine(-1);
+		}
+		return (rdo >= 4 || fullBoard());
 	}
         
         	/**
@@ -58,15 +92,15 @@ public class BoardControl {
 	 * post. Un valor entero, representando el mayor numero de fichas del mismo tipo alineadas 
 	 * consecutivamente en forma vertical, es retornado.
 	 */
-	private static int maxLineV(int[][] board, int x) { // x=1 | x=-1
+	private int maxLineV(int x) { // x=1 | x=-1
 		int maxV = 0;
 		int count = 0;
 		int col = 0;
-		while (col < board[0].length) {
-			int row = board.length-1; // board.length = nro de filas
+		while (col < table[0].length) {
+			int row = table.length-1; // board.length = nro de filas
 			count = 0;
-			while (row >= 0 && (board[row][col] != 0)) {
-				if (board[row][col] != x) {
+			while (row >= 0 && (table[row][col] != 0)) {
+				if (table[row][col] != x) {
 					maxV = Math.max(maxV, count);
 					count = 0;
 				}
@@ -90,15 +124,15 @@ public class BoardControl {
 	 * post. Un valor entero, representando el mayor numero de fichas del mismo tipo alineadas 
 	 * consecutivamente en forma horizontal, es retornado.
 	 */
-	private static int maxLineH(int[][] board, int x) {
-		int maxH = 0;
+	private int maxLineH(int x) {
+                int maxH = 0;
 		int count = 0;
-		int row = board.length-1;
+		int row = table.length-1;
 		while (row >= 0) {
 			int col = 0;
 			count = 0;
-			while (col < board[0].length) {
-				if (board[row][col] != x) {
+			while (col < table[0].length) {
+				if (table[row][col] != x) {
 					maxH = Math.max(maxH, count);
 					count = 0;
 				}
@@ -112,7 +146,7 @@ public class BoardControl {
 		return maxH;
 	}
 
-        // Ascendente y descendente en relación al barrido de columnas
+        // La posición (0,0) de la matríz es el extremo superior izquierdo 
         
 	/**
 	 * Computa el mayor numero de fichas del mismo tipo alineadas consecutivamente en una 
@@ -125,15 +159,15 @@ public class BoardControl {
 	 * post. Un valor entero, representando el mayor numero de fichas del mismo tipo alineadas 
 	 * consecutivamente en una diagonal descendente, es retornado.
 	 */
-	private static int maxLineDD(int[][] board, int x) {
-		int maxDD = 0;
+	private int maxLineDD(int x) {
+                int maxDD = 0;
 		int count = 0;
-		int row = board.length;
-		int col = board[0].length;
+		int row = table.length;
+		int col = table[0].length;
 		for (int n=0; n<row; n++) { // recorrido de la diagonal principal 
 			count = 0;		// y la parte inferior del tablero
 			for (int i=n, j=0; i<row && j<col; i++, j++) {
-				if (board[i][j] != x) {
+				if (table[i][j] != x) {
 					maxDD = Math.max(maxDD, count);
 					count = 0;
 				}
@@ -145,8 +179,8 @@ public class BoardControl {
 		}
 		for (int j=1; j<col; j++) { // recorrido de la parte superior del tablero
 			count = 0;
-			for (int i=0; i+j<6; i++) {
-				if (board[i][j+i] != x) {
+			for (int i=0; i+j<7; i++) {
+				if (table[i][j+i] != x) {
 					maxDD = Math.max(maxDD, count);
 					count = 0;
 				}
@@ -170,15 +204,15 @@ public class BoardControl {
 	 * post. Un valor entero, representando el mayor numero de fichas del mismo tipo alineadas 
 	 * consecutivamente en una diagonal ascendente, es retornado.
 	 */
-	private static int maxLineDA(int[][] board, int x) {
-		int maxDA = 0;
+	private int maxLineDA(int x) {
+                int maxDA = 0;
 		int count = 0;
-		int row = board.length;
-		int col = board[0].length;
+		int row = table.length;
+		int col = table[0].length;
 		for (int n=0; n<row; n++) { // recorrido de la diagonal principal y 
 			count = 0;		// la parte inferior del tablero
 			for (int i=n, j=col-1; i<row && j>=0; i++, j--) {
-				if (board[i][j] != x) {
+				if (table[i][j] != x) {
 					maxDA = Math.max(maxDA, count);
 					count = 0;
 				}
@@ -191,7 +225,7 @@ public class BoardControl {
 		for (int n=col-2; n>=0; n--) { // recorrido de la parte superior del tablero
 			count = 0;
 			for (int i=0, j=n; i<row && j>=0; i++, j--) {
-				if (board[i][j] != x) {
+				if (table[i][j] != x) {
 					maxDA = Math.max(maxDA, count);
 					count = 0;
 				}
@@ -213,15 +247,15 @@ public class BoardControl {
 	 * post. El mayor nro de fichas alineadas del tipo 'n' (ya sea en forma vertical, 
 	 * horizontal o diagonal) en la matriz 'board' es retornado.
 	 */
-	private static int maxLine(int[][] board, int n) {
+	private int maxLine(int n) {
 		int v, h, da, dd;
-		v = maxLineV(board,n);
-		h = maxLineH(board,n);
-		da = maxLineDA(board,n);
-		dd = maxLineDD(board,n);
+		v = maxLineV(n);
+		h = maxLineH(n);
+		da = maxLineDA(n);
+		dd = maxLineDD(n);
 		return (Math.max(Math.max(v,h), Math.max(da,dd)));
 	}
-
+/*
 	public void showBoard() {
 		System.out.println("************ Board *************");
 		String line = new String();
@@ -234,13 +268,28 @@ public class BoardControl {
 		  }
 		  System.out.println(line);
 		} //end for rows
-
-
-
-
+                
+            }
+  */      
+        	/**
+	 * Indica si un tablero esta completo, es decir, si no existen lugares libres en el mismo 
+	 * donde colocar una ficha. En este caso, no es posible realizar una nueva jugada.
+	 * @param m es la matriz representante del tablero.
+	 * @return true ssi m esta completo.
+	 * @pre. true.
+	 * @post. true es retornado ssi m esta completo.
+	 */
+	public boolean fullBoard() {
+		boolean rdo = true;
+		int col = 0;
+		int numCol = b.getGrid()[0].length;
+		while (rdo && col<numCol) {
+			rdo = rdo && (b.getGrid()[0][col] != 0);
+			col++;
+		}
+		return rdo;
 	}
+        
  
 
  }
-
-
