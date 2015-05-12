1f1c7ede902c2344d16b3cb7de029d178b524db4
grupomm <grupomarianomatias2015@gmail.com>
2015-05-12 12:06:33 -0300

    "Clase User atributos-metodos"

--- src/main/java/com/unrc/app/User.java
+++ src/main/java/com/unrc/app/User.java
@@ -3,8 +3,50 @@ package com.unrc.app;
 import org.javalite.activejdbc.Model;
 
 public class User extends Model {
-   static {
-    validatePresenceOf("first_name");	
+   String nickname ;
+   String mail;
+   String pass;
+    
+   
+   /*Constructor de la clase User
+   */
+   public User (){    
+       nickname ="";
+       mail="";
+       pass="";
+   
+   }
+   /* Set de la clase User
+   
+   */
+   public void setUser(String n, String m, String p){
+       nickname=n;
+       mail=m;
+       pass= p;
+   
+       
+    /* Gets de la clase User
+    */
+   
+   }
+   public String getUserNick (){
+       return nickname;
+   
+   }
+   public String getUserMail(){
+       return mail;
+   }
+    
+   public String getUserPass(){
+       return pass;
+   } 
+   public String getUserInfo(){
+       return nickname +" "+mail+" "+pass;
+   }
+    
+    static {
+    validatePresenceOf("first_name");
+    
   }
 
 }
